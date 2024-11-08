0f29d7965a5731386301e944fa0749eb0dec1462
DESKTOP-T3SRPQH\Skip <jesterwerks@gmail.com>
2024-11-08 14:41:39 -0600

    19.Use Supabase UI to Create Tables and Rows

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+/* eslint-env node */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint0config-prettier/skip-formatting',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/multi-word-component-names': 0,
+  },
+}
---
+++ src/pages/projects/[...catchAll].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('404 Not Found')
+</script>
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
