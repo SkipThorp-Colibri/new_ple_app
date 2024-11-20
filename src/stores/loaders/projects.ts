67ac75db1483b3a171fb5629a2713dc548006350
DESKTOP-T3SRPQH\Skip <jesterwerks@gmail.com>
2024-11-20 15:23:13 -0600

    96 working end

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -5,13 +5,25 @@ export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('cached and fresh data matched')
+          return
+        } else {
+          console.log('something has changed')
+          loadProjects.delete('projects')
+        }
+      })
+    }
+  }
   const getProjects = async () => {
-    if (projects.value?.length) return
-
     const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
     projects.value = data
+    validateCache()
   }
   return {
     projects,
