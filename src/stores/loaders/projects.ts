34166f93edfa361bc674a833516624f155663cc5
DESKTOP-T3SRPQH\Skip <jesterwerks@gmail.com>
2024-11-20 15:00:56 -0600

    95 working end useMemoize

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,12 +1,14 @@
 import { projectsQuery, type Projects } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
+  const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
   const getProjects = async () => {
     if (projects.value?.length) return
 
-    const { data, error, status } = await projectsQuery
+    const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
     projects.value = data
