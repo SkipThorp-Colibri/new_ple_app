6f80603b83ca4318f398e6e8c92e77b1b14efd69
DESKTOP-T3SRPQH\Skip <jesterwerks@gmail.com>
2024-11-20 08:26:52 -0600

    78 working end getting session data from localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
+import { supabase } from './lib/supabaseClient'
 
 const errorStore = useErrorStore()
+const authStore = useAuthStore()
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
+})
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getSession()
+  if (data.session?.user) await authStore.setAuth(data.session)
 })
 </script>
 
