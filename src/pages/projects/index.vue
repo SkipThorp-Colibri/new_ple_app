145cb677692af5b6a07e2a419cf84272353b9635
DESKTOP-T3SRPQH\Skip <jesterwerks@gmail.com>
2024-12-03 13:00:43 -0600

    126 working end Use Vue Meta to Set Dynamic Title and Meta Data

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -14,6 +14,13 @@ const { getGroupedCollabs, groupedCollabs } = useCollabs()
 getGroupedCollabs(projects.value ?? [])
 
 const columnsWithCollabs = columns(groupedCollabs)
+useMeta({
+  title: 'Projects | Pulse',
+  description: {
+    name: 'description',
+    content: 'Pulse is a ...',
+  },
+})
 </script>
 
 <template>
