cbb5566916b9aecb557d27d115f778d0c0388260
DESKTOP-T3SRPQH\Skip <jesterwerks@gmail.com>
2024-12-03 11:14:50 -0600

    122 working end Implement Global State with Composables

--- src/components/Layout/SidebarLinks.vue
+++ src/components/Layout/SidebarLinks.vue
@@ -4,7 +4,7 @@ interface LinkProp {
   to?: string
   icon: string
 }
-const props = defineProps<{
+defineProps<{
   links: LinkProp[]
 }>()
 
@@ -15,6 +15,7 @@ const emits = defineEmits<{
 const emitActionClicked = (linkTitle: string) => {
   emits('actionClicked', linkTitle)
 }
+const { menuOpen } = useMenu()
 </script>
 <template>
   <template v-for="link in links" :key="link.title">
@@ -23,19 +24,29 @@ const emitActionClicked = (linkTitle: string) => {
       exactActiveClass="text-primary bg-muted"
       :to="link.to"
       class="nav-link"
+      :class="{ 'justify-normal': menuOpen, 'justify-center': !menuOpen }"
     >
       <iconify-icon :icon="link.icon"></iconify-icon>
-      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+      <span class="text-nowrap" :class="{ block: menuOpen, hidden: !menuOpen }">{{
+        link.title
+      }}</span>
     </RouterLink>
-    <div v-else class="nav-link cursor-pointer" @click="emitActionClicked(link.title)">
+    <div
+      v-else
+      class="nav-link cursor-pointer"
+      @click="emitActionClicked(link.title)"
+      :class="{ 'justify-normal': menuOpen, 'justify-center': !menuOpen }"
+    >
       <iconify-icon :icon="link.icon"></iconify-icon>
-      <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+      <span class="text-nowrap" :class="{ block: menuOpen, hidden: !menuOpen }">{{
+        link.title
+      }}</span>
     </div>
   </template>
 </template>
 
 <style scoped>
 .nav-link {
-  @apply flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground;
+  @apply flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary text-muted-foreground;
 }
 </style>
